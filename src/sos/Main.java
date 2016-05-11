a693d8926d5c8010d2620719d3567cd9d3a5b08d
claudevanes <dvorskakamila@gmail.com>
2016-05-11 18:37:26 +0200

    Add comment.

--- src/sos/Main.java
+++ src/sos/Main.java
@@ -1,3 +1,6 @@
+/** 
+ * Thought of this project: 
+ */
 package sos;
 
 import java.util.logging.Logger;
@@ -5,20 +8,17 @@ import java.util.logging.Logger;
 public class Main {
 
 	public Main() {
-		// TODO Auto-generated constructor stub
+
 	}
 
-	 public final static Logger LOGGER = Logger.getLogger(Main.class.getName());
+	public final static Logger LOGGER = Logger.getLogger(Main.class.getName());
 
-	
 	public static void main(String[] args) {
-		
-		LOGGER.info("");
-		
-		System.out.println("In this programm is nothing.");
 
-		
-		
+		LOGGER.info("");
+
+		System.out.println("In this programm is nothing, like in others.");
+
 	}
 
 }
